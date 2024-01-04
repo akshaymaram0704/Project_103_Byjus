33fbf9ee74ff79cfa6897a332f8e77180cd58c2c
akshaymaram0704 <74571649+akshaymaram0704@users.noreply.github.com>
2024-01-04 12:50:03 -0600

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,50 @@
+import sys
+import time
+import random
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            print(f"Directory created: {event.src_path}")
+        else:
+            print(f"File created: {event.src_path}")
+
+    def on_modified(self, event):
+        if event.is_directory:
+            print(f"Directory modified: {event.src_path}")
+        else:
+            print(f"File modified: {event.src_path}")
+
+    def on_moved(self, event):
+        if event.is_directory:
+            print(f"Directory moved: {event.src_path} to {event.dest_path}")
+        else:
+            print(f"File moved: {event.src_path} to {event.dest_path}")
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            print(f"Directory deleted: {event.src_path}")
+        else:
+            print(f"File deleted: {event.src_path}")
+
+if __name__ == "__main__":
+    from_dir = "/Users/akshaymaram/Downloads"
+    event_handler = FileEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, from_dir, recursive=True)
+    observer.start()
+
+    print(f"Watching for file system events in: {from_dir}")
+    print("Press any key to stop the observer.")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
